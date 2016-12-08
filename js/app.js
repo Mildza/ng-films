e83bc6a1174b5beba1af72c79441b1beb3ec0051
Mildza Kuca <mildzan@gmail.com>
2016-12-08 23:14:34 +0100

    zanr

--- js/app.js
+++ js/app.js
@@ -11,4 +11,35 @@ $scope.orderByMe = function(by) {
   $scope.reverse = ($scope.by === by) ? !$scope.reverse : true;
       $scope.by = by;
 }
+
+
+$scope.genre = function(z) {
+
+    var niz = z.split(" ");
+    console.log(niz);
+    var y = niz.length;
+    if ( y > 1 ) {
+
+      for (var i = 0; i < y; i++) {
+      if (niz[i] = "komedija") {
+        
+      }
+
+      }
+      if (z === ) {
+        console.log(z);
+        return "akcija";
+
+      } else if (z === "drama") {
+        return "drama";
+      }
+
+    } else {
+      return z;
+    }
+
+
+};
+
+
 });
