0186f2de9388b015aa34ed283b5df166d30c27c8
Mildza Kuca <mildzan@gmail.com>
2016-12-11 23:21:54 +0100

    zanr

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngSanitize']);
 
-app.controller('movies', function($scope, $http) {
+
+app.controller('movies', function($scope, $http, $sce) {
     $http.get("http://mildza.freehostia.com/filmovi.php")
     .then(function (response) {$scope.filmovi = response.data.records;});
 
@@ -12,34 +13,50 @@ $scope.orderByMe = function(by) {
       $scope.by = by;
 }
 
+  $scope.genre = function(z) {
+      var niz = z.split(" ");
+      // console.log(niz);
+      var y = niz.length;
+      var val="";
+      if ( y > 1 ) {
+        for (var i = 0; i < y; i++) {
+          if (niz[i] == "akcija") {
+          val = "<img ng-src='images/akcija.png'>";
+        } else if (niz[i] == "komedija") {
+          val += "<img ng-src='images/komedija.png'>";
+        } else if (niz[i] == "triler") {
+          val += "<img ng-src='images/triler.png'>";
+        }
+        };
+        val = $sce.trustAsHtml(val);
 
-$scope.genre = function(z) {
+          return (val);
 
-    var niz = z.split(" ");
-    console.log(niz);
-    var y = niz.length;
-    if ( y > 1 ) {
+      };
+  };
 
-      for (var i = 0; i < y; i++) {
-      if (niz[i] = "komedija") {
-        
-      }
-
-      }
-      if (z === ) {
-        console.log(z);
-        return "akcija";
-
-      } else if (z === "drama") {
-        return "drama";
-      }
-
-    } else {
-      return z;
+  $scope.rank = function(stars) {
+    var mark;
+    if (stars == 5) {
+      mark = "ocena5";
+      return mark;
+    } else if (stars == 4) {
+      mark = "ocena4";
+      return mark;
+    } else if (stars == 3) {
+      mark = "ocena3";
+      return mark;
+    } else if (stars == 2) {
+      mark = "ocena2";
+      return mark;
+    } else if (stars == 1) {
+      mark = "ocena1";
+      return mark;
+    } else if (stars == 0) {
+      mark = "ocena0";
+      return mark;
     }
 
-
-};
-
+  };
 
 });
